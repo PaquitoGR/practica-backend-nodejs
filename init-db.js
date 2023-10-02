fa224d6bb73c01530e36784db84b9faa90cdacb7
Francisco Suárez <paco.max@gmail.com>
2023-10-02 16:42:00 +0200

    Added init-db and init-db-data.json

---
+++ init-db.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const readline = require('node:readline');
+const connection = require('./lib/connectMongoose');
+const Ad = require('./models/Ad');
+const initData = require('./init-db-data.json');
+
+main().catch(err => console.log('Error', err));
+
+function deleteQuestion (text) {
+  return new Promise((resolve, reject) => {
+    const ifc = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    ifc.question(text, answer => {
+      ifc.close();
+      resolve(answer.toLowerCase() === 'y');
+    });
+  });
+}
+
+async function main () {
+  await new Promise(resolve => connection.once('open', resolve));
+
+  const del = await deleteQuestion(
+    'Are you sure to delete the data base and load initial data? (y/n)'
+  );
+
+  if (!del) {
+    process.exit();
+  }
+
+  await initAds();
+  connection.close();
+}
+
+async function initAds () {
+  // delete all items in collection Ads
+  const deleted = await Ad.deleteMany();
+  console.log(`${deleted.deletedCount} ads deleted.`);
+
+  // create initial data
+  const inserted = await Ad.insertMany(initData.ads);
+  console.log(`${inserted.lenght} ads created.`);
+};
--- models/Ad.js
+++ models/Ad.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const adSchema = mongoose.Schema({
   name: { type: String, maxlenght: 20, index: true, required: true },
-  adType: { type: String, enum: ['sale', 'search'], default: 'sale' },
+  sale: { type: Boolean, default: true },
   price: { type: Number, index: true, min: 0, max: 1000000, required: true },
   img: { type: String, default: 'Picture not defined' },
   tags: { type: [String], index: true, enum: ['work', 'lifestyle', 'motor', 'mobile'] }
 });
 
-adSchema.statics.lista = function(filtro, skip, limit, sort, fields) {
+adSchema.statics.list = function (filtro, skip, limit, sort, fields) {
   const query = Ad.find(filtro);
   query.skip(skip);
   query.limit(limit);
